897192f7afd0b79fb53c4b36a6cca2c4767793ae
Ozan Çağlayan <ozancag@gmail.com>
2014-04-03 18:31:30 +0300

    Initial commit

---
+++ sts-fetch-query-urls.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import sys
+import bs4
+import urllib2
+
+VIEWSTATE = None
+
+def get_viewstate(url=None, html=None):
+    if url:
+        _html = bs4.BeautifulSoup(urllib2.urlopen(url).read())
+    else:
+        _html = html
+
+    return _html.find("input", attrs={"id" : "__VIEWSTATE"}).get("value")
+
+def get_states(city):
+    """Get POST parameters for getting states."""
+
+    global VIEWSTATE
+    VIEWSTATE = get_viewstate(url="http://sts.chp.org.tr")
+
+    params =  {
+                "__LASTFOCUS"       : "",
+                "__VIEWSTATE"       : VIEWSTATE,
+                "__EVENTTARGET"     : "drpIller",
+                "__EVENTARGUMENT"   : "",
+                "txtTckn"           : "",
+                "drpIller"          : city,
+              }
+
+    states = {}
+    r = requests.post("http://sts.chp.org.tr/", params)
+    html = bs4.BeautifulSoup(r.text)
+    for st in html.find("select", attrs={"id" : "drpIlceler"}).findAll("option")[1:]:
+        states[st.getText().strip()] = st.get("value")
+
+    VIEWSTATE = get_viewstate(html=html)
+
+    return states
+
+def get_boxes(city, state):
+    """Get POST parameters for ballot boxes using state info."""
+
+    params = {
+                "__LASTFOCUS"       : "",
+                "__VIEWSTATE"       : VIEWSTATE,
+                "__EVENTTARGET"     : "drpIlceler",
+                "__EVENTARGUMENT"   : "",
+                "txtTckn"           : "",
+                "drpIller"          : city,
+                "drpIlceler"        : state,
+             }
+
+    url_pattern = "http://sts.chp.org.tr/SonucDetay.aspx?sid="
+
+    r = requests.post("http://sts.chp.org.tr/", params)
+    html = bs4.BeautifulSoup(r.text)
+    urls = []
+
+    for sid in html.find("select", attrs={"id" : "drpSandiklar"}).findAll("option")[1:]:
+        urls.append("http://sts.chp.org.tr/SonucDetay.aspx?sid=" + sid.get("value"))
+
+    return urls
+
+if __name__ == "__main__":
+    # List of cities
+    cities = [
+     u'ADANA',
+     u'ADIYAMAN',
+     u'AFYONKARAH\u0130SAR',
+     u'A\u011eRI',
+     u'AMASYA',
+     u'ANKARA',
+     u'ANTALYA',
+     u'ARTV\u0130N',
+     u'AYDIN',
+     u'BALIKES\u0130R',
+     u'B\u0130LEC\u0130K',
+     u'B\u0130NG\xd6L',
+     u'B\u0130TL\u0130S',
+     u'BOLU',
+     u'BURDUR',
+     u'BURSA',
+     u'\xc7ANAKKALE',
+     u'\xc7ANKIRI',
+     u'\xc7ORUM',
+     u'DEN\u0130ZL\u0130',
+     u'D\u0130YARBAKIR',
+     u'ED\u0130RNE',
+     u'ELAZI\u011e',
+     u'ERZ\u0130NCAN',
+     u'ERZURUM',
+     u'ESK\u0130\u015eEH\u0130R',
+     u'GAZ\u0130ANTEP',
+     u'G\u0130RESUN',
+     u'G\xdcM\xdc\u015eHANE',
+     u'HAKKAR\u0130',
+     u'HATAY',
+     u'ISPARTA',
+     u'MERS\u0130N',
+     u'\u0130STANBUL',
+     u'\u0130ZM\u0130R',
+     u'KARS',
+     u'KASTAMONU',
+     u'KAYSER\u0130',
+     u'KIRKLAREL\u0130',
+     u'KIR\u015eEH\u0130R',
+     u'KOCAEL\u0130',
+     u'KONYA',
+     u'K\xdcTAHYA',
+     u'MALATYA',
+     u'MAN\u0130SA',
+     u'KAHRAMANMARA\u015e',
+     u'MARD\u0130N',
+     u'MU\u011eLA',
+     u'MU\u015e',
+     u'NEV\u015eEH\u0130R',
+     u'N\u0130\u011eDE',
+     u'ORDU',
+     u'R\u0130ZE',
+     u'SAKARYA',
+     u'SAMSUN',
+     u'S\u0130\u0130RT',
+     u'S\u0130NOP',
+     u'S\u0130VAS',
+     u'TEK\u0130RDA\u011e',
+     u'TOKAT',
+     u'TRABZON',
+     u'TUNCEL\u0130',
+     u'\u015eANLIURFA',
+     u'U\u015eAK',
+     u'VAN',
+     u'YOZGAT',
+     u'ZONGULDAK',
+     u'AKSARAY',
+     u'BAYBURT',
+     u'KARAMAN',
+     u'KIRIKKALE',
+     u'BATMAN',
+     u'\u015eIRNAK',
+     u'BARTIN',
+     u'ARDAHAN',
+     u'I\u011eDIR',
+     u'YALOVA',
+     u'KARAB\xdcK',
+     u'K\u0130L\u0130S',
+     u'OSMAN\u0130YE',
+     u'D\xdcZCE']
+
+    # Select city
+    try:
+        city = cities.index(sys.argv[1].decode("utf-8")) + 1
+    except IndexError, e:
+        print "Usage: %s <city_name (e.g. İSTANBUL)> [state_name (e.g. KADIKÖY)]" % sys.argv[0]
+        sys.exit(1)
+
+    # Get states of this city
+    state_dict = get_states(city)
+
+    if len(sys.argv) > 2:
+        # State is specified
+        state_name = sys.argv[2].decode("utf-8")
+        states = [state_dict[state_name]]
+        output = cities[city-1] + "_" + state_name + ".txt"
+    else:
+        # All of them
+        states = state_dict.values()
+        output = cities[city-1] + ".txt"
+
+    boxes = []
+    for state in states:
+        print state
+        boxes.extend(get_boxes(city, state))
+
+    open(output, "w").write("\n".join(boxes))
---
+++ sts-parse-urls-into-csv.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import bs4
+import urllib2
+
+BB, ILCE, MECLIS = range(1,4)
+
+headers = [
+            "il",
+            "ilce",
+            "sandik",
+            "alan",
+            "kayitli_secmen",
+            "oy_kullanan_kayitli_secmen",
+            "kanun_geregi_oy_kullanan",
+            "kullanilan_toplam_oy",
+            "itirazsiz_gecerli_oy",
+            "itirazli_gecerli_oy",
+            "gecerli_oy",
+            "gecersiz_oy",
+          ]
+
+def fetch_and_parse(url, get_headers=False):
+    html = bs4.BeautifulSoup(urllib2.urlopen(url, "r").read())
+    row = []
+
+    row.extend(html.find("span", attrs={"id":"TabContainer_TabPanel1_lblOzetIlIlce1"}).getText().replace(" ", "").split("/"))
+    row.append(html.find("span", attrs={"id":"TabContainer_TabPanel1_lblOzetSandikAlani1"}).getText())
+    # NOTE: Now for BB
+    row.extend([v.get("value", "0") for v in html.findAll("input", attrs={"class" : "chp-info-field"})[:8]])
+
+    # List of all vote results for BB
+    votes = [v for v in html.findAll("div", attrs={"class" : "chp-vote-row", "id" : None})\
+                     if "TabPanel1_rptPartiler" in v.input.get("id", "")]
+    for vote in votes:
+        # Get the name from the logo url (easiest)
+        name = "%s_oy" % (os.path.basename(vote.img["src"]).split(".")[0])
+        vote_count = vote.input.get("value", "0")
+        row.append(vote_count)
+        if get_headers:
+            headers.append(name)
+
+    return ",".join(row).encode("utf-8")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print "Usage: %s <list_of_sids>" % sys.argv[0]
+        sys.exit(1)
+
+    with open("results.csv", "w") as csv:
+        list_of_urls = open(sys.argv[1], "r").read().split("\n")
+        # Parse the first one to fetch columns
+        first = list_of_urls.pop(0)
+        row = fetch_and_parse(first, True)
+
+        csv.write(",".join(headers) + "\n")
+        csv.write(row + "\n")
+
+        i = 2
+        total = len(list_of_urls) + 1
+
+        for url in list_of_urls:
+            print "[%3d / %5d] %s" % (i, total, url)
+            i += 1
+            csv.write(fetch_and_parse(url) + "\n")
